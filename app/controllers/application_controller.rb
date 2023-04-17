4a3c24b0769b002c7d8d5a78d613b14b80dc6a77
nghialv <nghialv@kaopiz.com>
2023-04-17 16:27:23 +0700

    add signup,login,logout use devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:user_name])
+  end
+
+  def sign_in_params
+    params.require(:user).permit(:login, :password, :remember_me)
+  end
 end
