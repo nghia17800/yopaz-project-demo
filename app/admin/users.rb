847da537b0583297fbd27b28cccdc353b50a74a2
nghialv <nghialv@kaopiz.com>
2023-04-17 16:54:22 +0700

    add config list user, new user in admin page

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,18 +1,23 @@
 ActiveAdmin.register User do
+  permit_params :email, :user_name, :password, :password_confirmation
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :email, :encrypted_password, :user_name, :reset_password_token, :reset_password_sent_at, :remember_created_at
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:email, :encrypted_password, :user_name, :reset_password_token, :reset_password_sent_at, :remember_created_at]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  index do
+    selectable_column
+    id_column
+    column :email
+    column :user_name
+    column :created_at
+    column :updated_at
+    actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :email
+      f.input :user_name
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.actions
+  end
 end
